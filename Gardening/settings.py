9b5ffbcdef0aba20e8fcf78decded41dae424204
Alexander Musonda <musondaalexander97@gmail.com>
2023-02-20 14:47:09 +0200

    Made some changes to the settings file

--- Gardening/settings.py
+++ Gardening/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-rp+&b1gvb9%^nm429@7%crc45lnxczv^svai!$70xk6c*k=bv!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
